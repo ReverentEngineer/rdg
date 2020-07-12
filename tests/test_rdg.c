0fe073f986c9fe368ec9e69e8f3d9efed03e2110
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-12 09:26:02 -0400

    Add more robust tests of rdg_generate.

--- tests/test_rdg.c
+++ tests/test_rdg.c
@@ -5,18 +5,78 @@
 #endif
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
   struct rdg *rdg = NULL;
+  unsigned char *data = NULL;
+  size_t size = 0;
   
   rdg = rdg_new("test[123]");
   assert(rdg != NULL);
   assert(rdg_permutations(rdg) == 3);
+
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 5);
+  assert(data != NULL);
+  assert(memcmp("test1", data, 5) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 5);
+  assert(data != NULL);
+  assert(memcmp("test2", data, 5) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 5);
+  assert(data != NULL);
+  assert(memcmp("test3", data, 5) == 0);
+  assert(!rdg_generate(&data, &size, rdg));
+  assert(data == NULL);
+  assert(size == 0);
   (void) rdg_free(rdg);
 
   rdg = rdg_new("test[123][abc]");
   assert(rdg != NULL);
   assert(rdg_permutations(rdg) == 9);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test1a", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test1b", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test1c", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test2a", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test2b", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test2c", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test3a", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test3b", data, 6) == 0);
+  assert(rdg_generate(&data, &size, rdg));
+  assert(size == 6);
+  assert(data != NULL);
+  assert(memcmp("test3c", data, 6) == 0);
+  assert(!rdg_generate(&data, &size, rdg));
+  assert(data == NULL);
+  assert(size == 0);
+
+
   (void) rdg_free(rdg);
 
   return 0;
